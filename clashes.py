e0b69312d423f85293a617f5477e0abdea726bb1
misspawty <flora.roumpani.11@ucl.ac.uk>
2021-04-28 18:14:20 +0100

    fix bug in clashes (duplicate error)

--- clashes.py
+++ clashes.py
@@ -17,10 +17,11 @@ import math
 import Grasshopper.Kernel as gh
 
 
+
 x,y,z = [],[],[]
-clash, breakbond = [],[]
+c,b, = [],[]
 #clash_th = 3.75
-#break_th = 2.7
+#break_th = 5
 
 def SplitCoordinates(xyz):
     for i in range (len(xyz)):
@@ -30,33 +31,39 @@ def SplitCoordinates(xyz):
         z.append(float(z1))
 
 def CheckClash(xyz,x,y,z):
+    error = 0
     for i in range (len(CA_xyz)):
         for j in range (len(CA_xyz)):
             if (i != j):
                 dist = math.sqrt((x[j]-x[i])**2 + (y[j]-y[i])**2 + (z[j]-z[i])**2)
                 if (dist < clash_th):
-                    clash.append(CA_xyz[i])
-                    Error1("Clash: ", CA_xyz[i])
+                    c.append(CA_xyz[i])
+                    error=1
+    if (error == 1):
+        print clash
+        Error1("Clash: ", CA_xyz[0])
+        return(set(c))
+
 
 def CheckBreak(xyz,x,y,z):
+    error = 0
     for i in range (len(xyz)):
         if (i+1 < len(xyz)):
             dist = math.sqrt((x[i+1]-x[i])**2 + (y[i+1]-y[i])**2 + (z[i+1]-z[i])**2)
-            if (dist < break_th):
-                breakbond.append(CA_xyz[i])
-                Error1("Break: ", CA_xyz[i])
+            if (dist > break_th):
+                b.append(CA_xyz[i])
+                error=1
+    if (error == 1):
+        Error1("Break: ", CA_xyz[0])
+        return (set(b))
 
+        
 def Error1(m, d) :
-    e= gh.GH_RuntimeMessageLevel.Error
+    e = gh.GH_RuntimeMessageLevel.Error
     ghenv.Component.AddRuntimeMessage(e, m + str(d))
 
 
 if __name__ == "__main__": 
     SplitCoordinates(CA_xyz)
-    CheckClash(CA_xyz,x,y,z)
-    CheckBreak(CA_xyz,x,y,z)
-
-
-
-
-
+    clash = CheckClash(CA_xyz,x,y,z)
+    breakbond = CheckBreak(CA_xyz,x,y,z)
